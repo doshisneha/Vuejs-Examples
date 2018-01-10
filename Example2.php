fd7d220353606d525130b46eee9bf1428baf79c6
doshisneha <doshisneha64@gmail.com>
2018-01-09 23:08:50 -0800

    Example 2

---
+++ Example2.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Example1</title>
+  <script src="https://unpkg.com/vue@2.5.13/dist/vue.js"></script>
+</head>
+<body>
+
+
+  <div id="app" style="margin-top:10%;margin-left:40%">
+    <input type="number" v-model="v1" placeholder="Enter Value 1" > 
+    <input type="number" v-model="v2" placeholder="Enter Value 2" >
+    <br><br>
+    <button v-on:click="sum">ADD</button>  {{add}}
+    <br><br>
+    <button v-on:click="subustract">Deduct</button>  {{sub}}
+    <br><br>
+    <button v-on:click="multiply">Multiply</button>  {{multi}}
+    <br><br>
+    <button v-on:click="devide">Devide</button> {{div}}
+  </div>
+  <script>
+    var app = new Vue({
+      el: '#app',
+      data: {
+        v1:'',
+        v2:'',
+        add: '',
+        sub:'',
+        multi:'',
+        div:'',
+      },
+      methods:
+      {
+          sum:function()
+          {
+            if(this.v1!='' && this.v2!='')
+            {
+              this.add=parseInt(this.v1)+parseInt(this.v2);
+              
+            } 
+            else
+            {
+                alert("Both Input Are Complusary");
+            }
+          },
+          subustract:function()
+          {
+            if(this.v1!='' && this.v2!='')
+            {
+              this.sub=parseInt(this.v1)-parseInt(this.v2);
+            } 
+            else
+            {
+                alert("Both Input Are Complusary");
+            }
+          },
+          multiply:function()
+          {
+            if(this.v1!='' && this.v2!='')
+            {
+              this.multi=parseInt(this.v1)*parseInt(this.v2);
+            } 
+            else
+            {
+                alert("Both Input Are Complusary");
+            }
+          },
+          devide:function()
+          {
+            if(this.v1!='' && this.v2!='')
+            {
+              this.div=parseInt(this.v1)/parseInt(this.v2);
+            } 
+            else
+            {
+                alert("Both Input Are Complusary");
+            }
+          },
+      },
+    })
+
+  </script>
+
+
+
+  </body>
+  </html>
