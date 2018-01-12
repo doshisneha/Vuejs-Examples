f04021bb7bb9b968888cf769e49a7c3d8b5796f9
doshisneha <doshisneha64@gmail.com>
2018-01-11 23:59:59 -0800

    Example 6

--- Example6.php
+++ Example6.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Vue.js Inline Editor</title>
+    <title>Example 6</title>
 
     <style>
     /* Hide un-compiled mustache bindings
